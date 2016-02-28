73ae304b48036a14cc141306cc4ac19d3ac2e6c1
TPei <t.s.peikert@gmail.com>
2016-02-28 13:37:29 +0100

    adds rspec model tests

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Level, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'needs a title' do
+    level = Level.new
+    expect(level).not_to be_valid
+    expect { level.save }.to_not change{Level.count}
+    level.title = 'some title'
+    expect(level).to be_valid
+    expect { level.save }.to change{Level.count}.from(0).to(1)
+  end
 end
