9916dd943821448f49062162258cb1e7dbc02466
TPei <t.s.peikert@gmail.com>
2016-02-28 13:14:11 +0100

    validations, default values for operator

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,2 +1,9 @@
 class Operator < ActiveRecord::Base
+  validates :title, :operator_name, :sign, :min_number, :max_number, presence: true
+  after_create :post_init
+
+  def post_init
+    self.is_unary = false if self.is_unary.nil?
+    self.floats_allowed = false if self.floats_allowed.nil?
+  end
 end
